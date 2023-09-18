5ccbb4ed6746bd606101194273aef30db0579264
Estefane1992 <fannyczs2012@gmail.com>
2023-09-18 18:47:49 -0300

    novos arquivos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,36 @@
-import { Container, Title } from "./styles";
+import { Header } from "@components/Header";
+import { Container} from "./styles";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export default function Groups(){
+    const [groups, setGroups] = useState<string[]>([]);
     return(
         <Container>
-            <Title>Criar grupos</Title>
+            <Header />
+            <Highlight 
+                title="Turmas"
+                subtitle="Estude com sua turma"
+            />
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard 
+                        title={item}
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && {flex: 1}}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="Você ainda não possui turmas. Cadastre uma nova turma!"
+                    />
+                )}
+            />
         </Container>
+        
     )
 }
